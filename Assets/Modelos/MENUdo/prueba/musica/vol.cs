4a3af7adc219ed3d73ef5e730b7f9bc0188ed7c1
leandroowow <164834820+leandroowow@users.noreply.github.com>
2025-06-11 06:15:45 -0400

    a

---
+++ Assets/Modelos/MENUdo/prueba/musica/vol.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OptionsController : MonoBehaviour
+{
+    [Header("UI Components")]
+    [SerializeField] private Slider sliderVolumen;
+    [SerializeField] private string nombreEscenaMenu = "MenuPrincipal";
+
+    private const string PREF_VOL_KEY = "VolumenGlobal";
+
+    void Start()
+    {
+        // Carga el volumen guardado (o usa 1 si no existe)
+        float vol = PlayerPrefs.GetFloat(PREF_VOL_KEY, 1f);
+        sliderVolumen.value = vol;
+        AudioListener.volume = vol;
+
+        // Suscribe el callback
+        sliderVolumen.onValueChanged.AddListener(OnVolumenChanged);
+    }
+
+    void OnDestroy()
+    {
+        // Evita fugas de memoria
+        sliderVolumen.onValueChanged.RemoveListener(OnVolumenChanged);
+    }
+
+    private void OnVolumenChanged(float nuevoVol)
+    {
+        AudioListener.volume = nuevoVol;                    // Ajusta volumen global
+        PlayerPrefs.SetFloat(PREF_VOL_KEY, nuevoVol);       // Guarda en disco
+        PlayerPrefs.Save();
+    }
+
+    // Asigna este método al botón “Volver”
+    public void VolverAlMenu()
+    {
+        SceneManager.LoadScene(nombreEscenaMenu);
+    }
+}
